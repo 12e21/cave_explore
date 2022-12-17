a708b0aa17859246527aa9aeddd7e68e92a00f10
12e21 <2496813401@qq.com>
2022-12-17 16:22:08 +0800

    创建了human类

---
+++ Human.h
@@ -0,0 +1,15 @@
+#include "Charactor.h"
+#ifndef CAVE_EXPLORE_HUMAN_H
+#define CAVE_EXPLORE_HUMAN_H
+
+
+class Human:Charactor {
+public:
+    Human();
+    ~Human();
+private:
+
+};
+
+
+#endif //CAVE_EXPLORE_HUMAN_H
